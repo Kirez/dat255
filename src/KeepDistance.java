61209f2a0f78359d20258a69791f47940e22e1e1
Arvid Wiklund <arvidwi@student.chalmers.se>
2017-09-22 14:48:49 +0200

    Fix JavaDoc Fixed JavaDoc for the KeepDistance.java interface. Added addtional descriptions of the documentation for readSensor, readSpeed, setSpeed.

--- src/KeepDistance.java
+++ src/KeepDistance.java
@@ -1,23 +1,30 @@
+/**
+ * This interface is supposed create basic methods to get data from the MOPED using the CarAPI.
+ * @author Timmy Truong, Arvid Wiklund, Hamza Kadric
+ */
 public interface KeepDistance {
 
   /**
-   * Funktionsbeskrivning
+   * Read sensor
+   * Read data from the sensor to get current distance to object in front.
    *
-   * @return värdet vi vill ha
+   * @return Double value of the distance read from sensor
    */
   public double readSensor();
 
   /**
-   * Funktionsbeskrivning 2
+   * Set speed
+   * Write a double to the MOPEDs engine in order to change the speed.
    *
-   * @param speed speeden vi vill sätta
+   * @param speed corresponding to the new speed
    */
   public void setSpeed(double speed);
 
   /**
-   * Funktionsbeskrivning 3
-   *
-   * @return speed of the car in m/s
+   * Read speed
+   * Read data from the odometer to get the current speed of the MOPED.
+   * 
+   * @return Speed of the car in m/s
    */
   public double readSpeed();
 }
