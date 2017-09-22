70e32a1078a7f7d377bc3cffc2eea5d730597ad1
Tagonice <Tagon1228@gmail.com>
2017-09-22 15:10:49 +0200

    Fix JavaDoc
    Cleaned up some minor grammar mistakes and tried to copy and follow the JavaDoc guideline.

--- src/KeepDistance.java
+++ src/KeepDistance.java
@@ -5,26 +5,23 @@
 public interface KeepDistance {
 
   /**
-   * Read sensor
-   * Read data from the sensor to get current distance to object in front.
+   * Reads data from the sensor to get current distance to object in front.
    *
-   * @return Double value of the distance read from sensor
+   * @return Double value of the distance read from sensor in mm.
    */
-  public double readSensor();
+  double readSensor();
 
   /**
-   * Set speed
-   * Write a double to the MOPEDs engine in order to change the speed.
+   * Writes a double to the MOPEDs engine in order to change the speed.
    *
-   * @param speed corresponding to the new speed
+   * @param speed corresponding to the new speed in m/s.
    */
-  public void setSpeed(double speed);
+  void setSpeed(double speed);
 
   /**
-   * Read speed
-   * Read data from the odometer to get the current speed of the MOPED.
+   * Reads data from the odometer to get the current speed of the MOPED.
    * 
    * @return Speed of the car in m/s
    */
-  public double readSpeed();
+  double getSpeed();
 }
