8b87ebf3263520117fc2d6759383b9270b9f06df
knya <johsvenn@student.chalmers.se>
2017-09-26 13:38:19 +0200

    renamed KeepDistance to Moveable

---
+++ src/Moveable.java
@@ -0,0 +1,24 @@
+/**
+ * This interface is supposed create basic methods to get data from the MOPED using the CarAPI.
+ * @author Hamza Kadric
+ * @author Timmy Truong
+ * @author Arvid Wiklund
+ */
+public interface Moveable {
+
+  /**
+   * Set speed
+   * Write a double to the MOPEDs engine in order to change the speed.
+   *
+   * @param speed corresponding to the new speed
+   */
+  void setSpeed(double speed);
+
+  /**
+   * Read speed
+   * Read data from the odometer to get the current speed of the MOPED.
+   *
+   * @return speed of the car in cm/s
+   */
+  double getSpeed();
+}
