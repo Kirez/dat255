2bfb8888350a760ecc1e3e83d3f62604ebc29f00
knya <johsvenn@student.chalmers.se>
2017-09-29 14:30:17 +0200

    added dumb Sensor.java, currently only calculates the distance between two Cars, does not reflect a proper Sensor

--- src/simulator/Sensor.java
+++ src/simulator/Sensor.java
@@ -1,7 +1,9 @@
 package simulator;
 
-class Sensor {
+public class Sensor {
 
-
+    public double getDistanceTo(Car car1, Car car2) {
+        return Math.sqrt(Math.pow(car2.getX() - car1.getX(), 2) + Math.pow(car2.getY() - car1.getY(), 2));
+    }
 
 }
