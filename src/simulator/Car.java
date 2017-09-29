67378896ddd3efed7f21d1cf52a4e59a2ccd144f
knya <johsvenn@student.chalmers.se>
2017-09-29 14:29:14 +0200

    added Car-class with some basic functionality

--- src/simulator/Car.java
+++ src/simulator/Car.java
@@ -1,13 +1,16 @@
 package simulator;
 
-class Car {
+public class Car {
     private double speed;
     private boolean isLeadingCar;
 
     private Regulator regulator;
     private Sensor sensor;
 
-    Car(boolean isLeadingCar) {
+    private double x;
+    private double y;
+
+    public Car(boolean isLeadingCar) {
         this.isLeadingCar = isLeadingCar;
 
         if (isLeadingCar) {
@@ -16,4 +19,25 @@ class Car {
         }
     }
 
+    public Car(double x, double y, boolean isLeadingCar) {
+        this(isLeadingCar);
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getDistanceTo(Car c) {
+        return sensor.getDistanceTo(this, c);
+    }
+
+    public void move(double speed) {
+        this.x = this.x + speed;
+    }
 }
