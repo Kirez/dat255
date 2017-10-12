116fe5fb57cbfddcdb6cd5a10ddefb47f6f5b0bc
knya <johsvenn@student.chalmers.se>
2017-10-12 20:20:29 +0200

    added base components for a desktop client user interface built in JavaFX

---
+++ platooning/src/main/java/ui/controllers/MainController.java
@@ -0,0 +1,28 @@
+package ui.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+/**
+ * Created by Johan on 2017-10-12.
+ */
+public class MainController implements Initializable {
+
+    @FXML Button connectButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    public void connectButtonActionPerformed(ActionEvent actionEvent) {
+
+    }
+}
